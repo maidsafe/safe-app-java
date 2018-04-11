e5eb309dcfbef5306332e85e80f09865216281d8
Lionel Faber <lionel1704@gmail.com>
2018-04-11 11:49:01 +0530

    refactor/safe-app: refactor tests for safe-app
    refactor tests for MData

--- safe-app/src/test/java/net/maidsafe/api/MDataTest.java
+++ safe-app/src/test/java/net/maidsafe/api/MDataTest.java
@@ -11,6 +11,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MDataTest {
     static {
@@ -31,6 +32,8 @@ public class MDataTest {
         client.mDataEntries.insert(entriesHandle, "someKey".getBytes(), "someValue".getBytes()).get();
         client.mData.put(mDataInfo, permissionHandle, entriesHandle).get();
 
+
+
         NativeHandle actionHandle = client.mDataEntryAction.newEntryAction().get();
         client.mDataEntryAction.insert(actionHandle, "someKey2".getBytes(), "someValue2".getBytes()).get();
         client.mData.mutateEntries(mDataInfo, actionHandle).get();
@@ -45,29 +48,35 @@ public class MDataTest {
         byte[] updatedValue = Helper.randomAlphaNumeric(10).getBytes();
         client.mDataEntryAction.update(actionHandle, entry.getKey().getVal(), updatedValue, entry.getValue().getEntryVersion() + 1).get();
         client.mData.mutateEntries(mDataInfo, actionHandle).get();
+        entriesHandle = client.mData.getEntriesHandle(mDataInfo).get();
+        entries = client.mDataEntries.listEntries(entriesHandle).get();
+
+        entry = entries.get(0);
         MDataValue value = client.mData.getValue(mDataInfo, entry.getKey().getVal()).get();
         Assert.assertEquals(new String(updatedValue), new String(value.getContent()));
         // Delete
         actionHandle = client.mDataEntryAction.newEntryAction().get();
-        client.mDataEntryAction.delete(actionHandle, entry.getKey().getVal(), value.getEntryVersion() + 1).get();
+        client.mDataEntryAction.delete(actionHandle, entry.getKey().getVal(), entry.getValue().getEntryVersion() + 1).get();
+        client.mData.mutateEntries(mDataInfo, actionHandle).get();
 
+        entriesHandle = client.mData.getEntriesHandle(mDataInfo).get();
         entries = client.mDataEntries.listEntries(entriesHandle).get();
-        Assert.assertEquals(1, entries.size());
+
+
+        Assert.assertEquals(entries.get(0).getValue().getContentLen(), 0);
 
         long serialisedSize = client.mData.getSerialisedSize(mDataInfo).get();
         byte[] serialisedMData = client.mData.serialise(mDataInfo).get();
         Assert.assertEquals(serialisedSize, serialisedMData.length);
         mDataInfo = client.mData.deserialise(serialisedMData).get();
-        entriesHandle = client.mData.getEntriesHandle(mDataInfo).get();
-        entries = client.mDataEntries.listEntries(entriesHandle).get();
-        Assert.assertEquals(1, entries.size());
         long version = client.mData.getVersion(mDataInfo).get();
         Assert.assertEquals(3, version);
+
         List<MDataKey> keys = client.mData.getKeys(mDataInfo).get();
         List<MDataValue> values = client.mData.getValues(mDataInfo).get();
         Assert.assertEquals(keys.size(), values.size());
-        Assert.assertEquals(entries.get(0).getKey(), keys.get(0));
-        Assert.assertEquals(entries.get(0).getValue(), values.get(0).getContent());
+        Assert.assertEquals(new String(entries.get(0).getKey().getVal()), new String(keys.get(0).getVal()));
+        Assert.assertEquals(new String(entries.get(0).getValue().getContent()), new String(values.get(0).getContent()));
 
         PermissionSet permissions = client.mData.getPermissionForUser(client.crypto.getAppPublicSignKey().get(), mDataInfo).get();
         Assert.assertTrue(permissions.getInsert());
@@ -77,7 +86,7 @@ public class MDataTest {
         Assert.assertFalse(permissions.getManagePermission());
     }
 
-    private void privateMDataCrud(MDataInfo mDataInfo) throws Exception {
+    private void privateMDataCrud(MDataInfo mDataInfo) throws Exception{
         NativeHandle nativeHandle = TestHelper.createTestApp(Helper.APP_ID).get();
         Client client = new Client(new AppHandle(nativeHandle.toLong()), new DisconnectListener());
         NativeHandle permissionHandle = client.mDataPermission.newPermissionHandle().get();
@@ -85,6 +94,7 @@ public class MDataTest {
         permissionSet.setInsert(true);
         permissionSet.setUpdate(true);
         permissionSet.setRead(true);
+        permissionSet.setDelete(true);
         client.mDataPermission.insert(permissionHandle, client.crypto.getAppPublicSignKey().get(), permissionSet).get();
         NativeHandle entriesHandle = client.mDataEntries.newEntriesHandle().get();
         byte[] key = client.mData.encryptEntryKey(mDataInfo, "someKey".getBytes()).get();
@@ -101,22 +111,29 @@ public class MDataTest {
         entriesHandle = client.mData.getEntriesHandle(mDataInfo).get();
         List<MDataEntry> entries = client.mDataEntries.listEntries(entriesHandle).get();
         Assert.assertEquals(2, entries.size());
-
         // Update
         actionHandle = client.mDataEntryAction.newEntryAction().get();
         MDataEntry entry = entries.get(0);
         byte[] updatedValue = Helper.randomAlphaNumeric(10).getBytes();
-        client.mDataEntryAction.update(actionHandle, entry.getKey().getVal(), updatedValue, entry.getValue().getEntryVersion() + 1).get();
+        byte[] encryptedUpdatedValue = client.mData.encryptEntryValue(mDataInfo, updatedValue).get();
+        client.mDataEntryAction.update(actionHandle, entry.getKey().getVal(), encryptedUpdatedValue, entry.getValue().getEntryVersion() + 1).get();
         client.mData.mutateEntries(mDataInfo, actionHandle).get();
+
         MDataValue entryValue = client.mData.getValue(mDataInfo, entry.getKey().getVal()).get();
         byte[] decryptedValue = client.mData.decrypt(mDataInfo, entryValue.getContent()).get();
         Assert.assertEquals(new String(updatedValue), new String(decryptedValue));
         // Delete
-        actionHandle = client.mDataEntryAction.newEntryAction().get();
-        client.mDataEntryAction.delete(actionHandle, entry.getKey().getVal(), entryValue.getEntryVersion() + 1).get();
-
+        entriesHandle = client.mData.getEntriesHandle(mDataInfo).get();
         entries = client.mDataEntries.listEntries(entriesHandle).get();
-        Assert.assertEquals(1, entries.size());
+
+        entry = entries.get(0);
+        actionHandle = client.mDataEntryAction.newEntryAction().get();
+        client.mDataEntryAction.delete(actionHandle, entry.getKey().getVal(), entry.getValue().getEntryVersion() + 1).get();
+        client.mData.mutateEntries(mDataInfo, actionHandle).get();
+
+        entriesHandle = client.mData.getEntriesHandle(mDataInfo).get();
+        entries = client.mDataEntries.listEntries(entriesHandle).get();
+        Assert.assertEquals(0, entries.get(0).getValue().getContentLen());
 
         long serialisedSize = client.mData.getSerialisedSize(mDataInfo).get();
         byte[] serialisedMData = client.mData.serialise(mDataInfo).get();
@@ -124,17 +141,17 @@ public class MDataTest {
         mDataInfo = client.mData.deserialise(serialisedMData).get();
         entriesHandle = client.mData.getEntriesHandle(mDataInfo).get();
         entries = client.mDataEntries.listEntries(entriesHandle).get();
-        Assert.assertEquals(1, entries.size());
+        Assert.assertEquals(2, entries.size());
     }
 
-//    @Test
-//    public void randomPublicMDataCRUDTest() throws Exception {
-//        NativeHandle nativeHandle = TestHelper.createTestApp(Helper.APP_ID).get();
-//        Client client = new Client(new AppHandle(nativeHandle.toLong()), new DisconnectListener());
-//        long tagType = 15001;
-//        MDataInfo mDataInfo = client.mData.getRandomPublicMData(tagType).get();
-//        publicMDataCrud(mDataInfo);
-//    }
+    @Test
+    public void randomPublicMDataCRUDTest() throws Exception {
+        NativeHandle nativeHandle = TestHelper.createTestApp(Helper.APP_ID).get();
+        Client client = new Client(new AppHandle(nativeHandle.toLong()), new DisconnectListener());
+        long tagType = 15001;
+        MDataInfo mDataInfo = client.mData.getRandomPublicMData(tagType).get();
+        publicMDataCrud(mDataInfo);
+    }
 
     @Test
     public void publicMDataCRUDTest() throws Exception {
@@ -147,25 +164,25 @@ public class MDataTest {
         publicMDataCrud(mDataInfo);
     }
 
-//    @Test
-//    public void randomPrivateMDataCRUDTest() throws Exception {
-//        NativeHandle nativeHandle = TestHelper.createTestApp(Helper.APP_ID).get();
-//        Client client = new Client(new AppHandle(nativeHandle.toLong()), new DisconnectListener());
-//        long tagType = 15001;
-//        MDataInfo mDataInfo = client.mData.getRandomPrivateMData(tagType).get();
-//        privateMDataCrud(mDataInfo);
-//    }
+    @Test
+    public void randomPrivateMDataCRUDTest() throws Exception {
+        NativeHandle nativeHandle = TestHelper.createTestApp(Helper.APP_ID).get();
+        Client client = new Client(new AppHandle(nativeHandle.toLong()), new DisconnectListener());
+        long tagType = 15001;
+        MDataInfo mDataInfo = client.mData.getRandomPrivateMData(tagType).get();
+        privateMDataCrud(mDataInfo);
+    }
 
-//    @Test
-//    public void privateMDataCRUDTest() throws Exception {
-//        NativeHandle nativeHandle = TestHelper.createTestApp(Helper.APP_ID).get();
-//        Client client = new Client(new AppHandle(nativeHandle.toLong()), new DisconnectListener());
-//        long tagType = 15001;
-//        EncryptKeyPair encryptKeyPair = client.crypto.generateEncryptKeyPair().get();
-//        byte[] nonce = client.crypto.generateNonce().get();
-//        byte[] secretKey = client.crypto.getRawSecretEncryptKey(encryptKeyPair.getSecretEncryptKey()).get();
-//        MDataInfo mDataInfo = client.mData.getPrivateMData(Helper.randomAlphaNumeric(Constants.XOR_NAME_LENGTH).getBytes(), tagType, secretKey, nonce).get();
-//        privateMDataCrud(mDataInfo);
-//    }
+    @Test
+    public void privateMDataCRUDTest() throws Exception {
+        NativeHandle nativeHandle = TestHelper.createTestApp(Helper.APP_ID).get();
+        Client client = new Client(new AppHandle(nativeHandle.toLong()), new DisconnectListener());
+        long tagType = 15001;
+        EncryptKeyPair encryptKeyPair = client.crypto.generateEncryptKeyPair().get();
+        byte[] nonce = client.crypto.generateNonce().get();
+        byte[] secretKey = client.crypto.getRawSecretEncryptKey(encryptKeyPair.getSecretEncryptKey()).get();
+        MDataInfo mDataInfo = client.mData.getPrivateMData(Helper.randomAlphaNumeric(Constants.XOR_NAME_LENGTH).getBytes(), tagType, secretKey, nonce).get();
+        privateMDataCrud(mDataInfo);
+    }
 
 }
