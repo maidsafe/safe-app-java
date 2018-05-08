ebb7c230a8a88b6870b58b3e51278b1af9e993b4
Lionel Faber <lionel1704@gmail.com>
2018-05-08 13:26:58 +0530

    push to elton

--- safe-app/src/main/java/net/maidsafe/api/Client.java
+++ safe-app/src/main/java/net/maidsafe/api/Client.java
@@ -5,6 +5,7 @@ import net.maidsafe.utils.OSInfo;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
@@ -33,16 +34,24 @@ public class Client extends Session {
             if (!generatedDir.mkdir()) {
                 throw new IOException("Failed to create temp directory " + generatedDir.getName());
             }
+            generatedDir.deleteOnExit();
+            System.setProperty("java.library.path", generatedDir.getAbsolutePath());
+
+            final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
+            sysPathsField.setAccessible(true);
+            sysPathsField.set(null, null);
+
+            System.out.println(System.getProperty("java.library.path"));
             File file = new File(generatedDir, baseLibName.concat(extension));
             file.deleteOnExit();
             InputStream inputStream = Client.class.getResourceAsStream("/native/".concat(baseLibName).concat(extension));
             Files.copy(inputStream, file.toPath());
-            System.load(file.getAbsolutePath());
+            System.loadLibrary(baseLibName);
             file = new File(generatedDir, libName.concat(extension));
             file.deleteOnExit();
             inputStream = Client.class.getResourceAsStream("/native/".concat(libName).concat(extension));
             Files.copy(inputStream, file.toPath());
-            System.load(file.getAbsolutePath());
+            System.loadLibrary(libName);
         } catch (Exception ex) {
             throw new ExceptionInInitializerError(ex);
         }
